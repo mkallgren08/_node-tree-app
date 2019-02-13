31ddd6a93bd3d2d1ba9bffc64536ff86d43096ca
mkallgren08 <mkallgren08@gmail.com>
2019-02-13 12:38:26 -0500

    Building for production

--- server.js
+++ server.js
@@ -114,9 +114,9 @@ db.once("open", function () {
 
 // Send every request to the React app
 // Define any API routes before this runs
-// app.get("*", function(req, res) {
-//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
-// });
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 app.listen(PORT, function() {
   console.log(`🌎 ==> Server now on port ${PORT}!`);
